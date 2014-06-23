8025686e7cc4f86addd6b4dc71e3ac54f061a5ef
Abigail Watson <awatson1978@gmail.com>
2014-06-22 23:47:14 -0400

    readme updates, package.js update, and shuffled app vs package locations

--- launch_nightwatch_from_velocity.sh
+++ launch_nightwatch_from_velocity.sh
@@ -1,9 +1,9 @@
 #!/bin//bash
 echo "installing nightwatch in .meteor/local/build"
-  cd .meteor/local/build
+  cd ../../.meteor/local/build
   sudo npm install nightwatch@0.5.3
-  cd ../../../
+  cd ../../../packages/selenium-nightwatch
 
 echo "running nightwatch"
 #  sudo .meteor/local/build/node_modules/nightwatch/bin/nightwatch -c private/nightwatch.json -t tests/leaderboardTest.js
-   sudo .meteor/local/build/node_modules/nightwatch/bin/nightwatch -c packages/selenium-nightwatch/nightwatch_from_app_root.json $1 $2
+   sudo ../../.meteor/local/build/node_modules/nightwatch/bin/nightwatch -c nightwatch_from_package.json $1 $2
--- package.js
+++ package.js
@@ -1,39 +1,20 @@
-// package.js
-// should go into the /meteor-project/packages/sample_package directory
 
 Package.describe({
-  // define a message to describe the package
   summary: "Dead-easy acceptance testing for your Meteor app with Selenium and Nightwatch.",
-
-  // for internal dependency packages, set the internal flag true
   internal: false
 });
 
-// If you're bundling an NPM package, be sure to reference the package as a dependency
-// Npm.depends({
-//   nightwatch: "0.5.3"
-// });
+
+Npm.depends({
+    'glob': '3.2.9',
+    'lodash': '2.4.1',
+    'rimraf': '2.2.8',
+    'xml2js': '0.4.2',
+    'meteor-stubs': '0.0.1'
+});
 
 Package.on_use(function (api) {
-
-  //var path = Npm.require('nightwatch');
-
-  // export the object
-
-  // if(typeof api.export !== 'undefined'){
-  //   api.export('NightwatchBridge', ['client', 'server']);
-  // }
-
-  //api.add_files(path.join('audio', 'click1.wav'), 'client');
-
-  // define dependencies using api.use
-  //api.use('package_name', 'directory/to/install/into');
-
-  // add files to specific locations using api.add_files
-  //api.add_files('library_name.js', 'directory/to/install/into');
-
-  // example: add multiple files to a location using an array
-  //api.add_files(['first_library.js', 'second_library.js'], 'client');
-
-  // example: add file to multiple locations using an array
+    api.use(['velocity', 'mirror']);
+    api.use('package-stubber');
+    api.add_files('velocity-integration.js', 'server');
 });
